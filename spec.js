a85578207625a9c21650c254c0388f5981695022
Iaroslav Rassokha <rassochaja88@gmail.com>
2021-04-09 17:07:04 +0200

    TC name change

--- spec.js
+++ spec.js
@@ -12,7 +12,7 @@ describe('Protractor Automation challenge', function () {
         expect(browser.getTitle()).toEqual(homePage.getPageTitle());
     })
 
-    it('Search for a product and assert that it could be bought', async function () {
+    it('Search for a product and assert that it is available for purchase', async function () {
         await homePage.searchForProduct("fressnapf");
 
         await searchResultPage.showAllSearchResults();
