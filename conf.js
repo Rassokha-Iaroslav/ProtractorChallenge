948eac2fe88a0fe17304b29a9240111d9969b643
Iaroslav Rassokha <rassochaja88@gmail.com>
2021-04-11 13:43:59 +0200

    reporter was added to check headless chrome errors

--- conf.js
+++ conf.js
@@ -1,4 +1,13 @@
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+    dest: 'target/screenshots',
+    filename: 'my-report.html',
+    captureOnlyFailedSpecs: true
+});
+
 exports.config = {
+
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['searchTests.js'],
@@ -6,12 +15,28 @@ exports.config = {
         {
             browserName: 'chrome',
             // chromeOptions: {
-            //     args: [ "--headless", "--disable-gpu"]
+            //     args: [ '--headless', '--disable-gpu', '--window-size=1920,1080', '--no-sandbox']
             // }
         }
         // ,
         // {
         //     browserName: 'firefox',
         // }
-    ]
+    ],
+
+    beforeLaunch: function() {
+        return new Promise(function(resolve){
+            reporter.beforeLaunch(resolve);
+        });
+    },
+
+    onPrepare: function() {
+        jasmine.getEnv().addReporter(reporter);
+    },
+
+    afterLaunch: function(exitCode) {
+        return new Promise(function(resolve){
+            reporter.afterLaunch(resolve.bind(this, exitCode));
+        });
+    }
 }
